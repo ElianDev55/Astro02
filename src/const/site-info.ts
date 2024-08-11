c093da70414012578d9ed90180d80f909865fa34
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2024-08-11 15:05:45 -0500

    Start

---
+++ src/const/site-info.ts
@@ -0,0 +1,4 @@
+export const siteInfo = {
+    title: 'Pokemon with Astro',
+    description: 'Pokemon with Astro is a simple web app that uses the Pokemon API to display a list of Pokemon and their details.',
+}
